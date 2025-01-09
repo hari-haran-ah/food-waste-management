f4d9b666a720a1da64385fe62e2e9802576895a7
gowrishan2 <gowrishangararl@gmail.com>
2025-01-09 19:39:04 +0530

    Footer updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer() {
     return (
       <footer className="bg-blue-800 p-4 text-center text-white">
-        <p>&copy; . All rights reserved.</p>
+        <p>&copy; 2025 All rights reserved.</p>
       </footer>
     );
   }
